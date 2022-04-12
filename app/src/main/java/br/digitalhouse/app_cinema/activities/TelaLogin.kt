cb32902125ad2b3beef9226a8f87511d42981667
Saulo Carvalho <saulocarvalho93@gmail.com>
2022-04-12 20:29:20 -0300

    Implanta tela login com codigo de validacao e classe usuario

---
+++ app/src/main/java/br/digitalhouse/app_cinema/activities/TelaLogin.kt
@@ -0,0 +1,52 @@
+package br.digitalhouse.app_cinema.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import br.digitalhouse.app_cinema.R
+import br.digitalhouse.app_cinema.data.Usuario
+
+class TelaLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_login)
+
+        var btnLogar = findViewById<Button>(R.id.btnLogarLogin)
+        btnLogar.setOnClickListener { validaBotaoLogar() }
+
+        var btnEsqueciSenha = findViewById<Button>(R.id.btnEsqueciSenhaLogin)
+        btnEsqueciSenha.setOnClickListener { intencaoTelaRecuperarSenha() }
+    }
+
+
+    fun validaBotaoLogar() {
+        var textSenhaLogar = findViewById<EditText>(R.id.editTextSenha)
+        val senhaInformada = textSenhaLogar.text.toString()
+        var textEmailLogar = findViewById<EditText>(R.id.editTextUsuario)
+        val loginInformado = textEmailLogar.text.toString()
+
+        val usuario = Usuario(loginInformado, senhaInformada)
+        val validaEmail: Boolean = usuario.validaEmail(loginInformado)
+        val validacao: Boolean = usuario.validaSenha(senhaInformada)
+        if (textEmailLogar.text.isNullOrBlank() || textSenhaLogar.text.isNullOrBlank()) {
+            Toast.makeText(this, "Preencha os campos", Toast.LENGTH_SHORT).show()
+        } else {
+            if (validacao && validaEmail) {
+                Toast.makeText(this, "${textEmailLogar.text} Logado!", Toast.LENGTH_SHORT).show()
+                var intent2 = Intent(this, TelaPrincipal::class.java)
+                startActivity(intent2)
+            } else {
+                Toast.makeText(this, "Dados incorretos", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+    fun intencaoTelaRecuperarSenha(){
+        var intent3 = Intent(this, TelaRecuperarSenha::class.java)
+        startActivity(intent3)
+    }
+}
---
+++ app/src/main/java/br/digitalhouse/app_cinema/data/Usuario.kt
@@ -0,0 +1,48 @@
+package br.digitalhouse.app_cinema.data
+
+data class Usuario(
+    val login: String,
+    val senha: String
+) {
+
+    fun validaEmail(login: String): Boolean {
+        var variavel = false
+        if (login.contains("@"))
+            variavel = true
+        return variavel
+    }
+
+    fun validaTamanho(senha: String): Boolean {
+        return senha.count() >= 8
+    }
+
+    fun validaEspeciais(senha: String): Boolean {
+        val listaEspeciais: List<String> =
+            listOf("!", "@", "#", "$", "¨", "&", "*", "(", ")", "'", "-", "=", "+", "_", ".", ",", "/", "%", ":", ";", "?")
+        var variavel = false
+        for (character in listaEspeciais)
+            if (senha.contains(character))
+                variavel = true
+        return variavel
+    }
+
+    fun validaNumeros(senha: String): Boolean {
+        val listaNumeros: List<String> =
+            listOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
+        var variavel = false
+        for (character in listaNumeros)
+            if (senha.contains(character))
+                variavel = true
+        return variavel
+    }
+
+    fun validaSenha(senha: String): Boolean {
+        val variavelEspeciais: Boolean = validaEspeciais(senha)
+        val variavelNumeros: Boolean = validaNumeros(senha)
+        val variavelTamanho: Boolean = validaTamanho(senha)
+        var variavel = false
+        if ((variavelEspeciais && variavelNumeros && variavelTamanho))
+            variavel = true
+        return variavel
+    }
+}
