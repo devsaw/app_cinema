46215bcf55640402de02a6d47eb6d4208905e69c
Saulo Carvalho <saulocarvalho93@gmail.com>
2022-04-12 20:36:17 -0300

    Implanta Tela Recuperar Senha

---
+++ app/src/main/java/br/digitalhouse/app_cinema/activities/TelaRecuperarSenha.kt
@@ -0,0 +1,13 @@
+package br.digitalhouse.app_cinema.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.digitalhouse.aplicativopi.R
+import br.digitalhouse.app_cinema.R
+
+class TelaRecuperarSenha : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_recuperar_senha)
+    }
+}
