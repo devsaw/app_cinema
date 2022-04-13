bfa55090f027092a1dc7131d0d1ae323e18bea77
Saulo Carvalho <saulocarvalho93@gmail.com>
2022-04-12 21:16:33 -0300

    Implanta Tela Redefinir Senha

--- app/src/main/java/br/digitalhouse/app_cinema/activities/TelaRedefinirSenha.kt
+++ app/src/main/java/br/digitalhouse/app_cinema/activities/TelaRedefinirSenha.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.digitalhouse.app_cinema.R
 
-class TelaRecuperarSenha : AppCompatActivity() {
+class TelaRedefinirSenha : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tela_recuperar_senha)
+        setContentView(R.layout.activity_tela_redefinir_senha)
     }
 }
