b04af5baf9e627560dd5cfc5506a5de913541111
Saulo Carvalho <saulocarvalho93@gmail.com>
2022-04-14 03:31:44 -0300

    Implanta Tela Para Assistir

---
+++ app/src/main/java/br/digitalhouse/app_cinema/activities/TelaParaAssistir.kt
@@ -0,0 +1,12 @@
+package br.digitalhouse.app_cinema.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.digitalhouse.app_cinema.R
+
+class TelaParaAssistir : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_para_assistir)
+    }
+}
