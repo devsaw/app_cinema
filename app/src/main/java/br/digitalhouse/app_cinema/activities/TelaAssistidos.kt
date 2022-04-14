b0905c91343931440df1376f64cb1c610ee74ac1
Saulo Carvalho <saulocarvalho93@gmail.com>
2022-04-14 03:38:13 -0300

    Implanta Tela Assistidos

---
+++ app/src/main/java/br/digitalhouse/app_cinema/activities/TelaAssistidos.kt
@@ -0,0 +1,12 @@
+package br.digitalhouse.app_cinema.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.digitalhouse.app_cinema.R
+
+class TelaAssistidos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_assistidos)
+    }
+}
