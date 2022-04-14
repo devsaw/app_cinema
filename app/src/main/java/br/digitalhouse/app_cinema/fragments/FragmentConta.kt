3737d701100ab2d12615109f9a72196fb871d293
Saulo Carvalho <saulocarvalho93@gmail.com>
2022-04-14 05:16:45 -0300

    Criação dos fragments e conclusão do fragment_filmes.xml com RecyclerView

---
+++ app/src/main/java/br/digitalhouse/app_cinema/fragments/FragmentConta.kt
@@ -0,0 +1,7 @@
+package br.digitalhouse.app_cinema.fragments
+
+import androidx.fragment.app.Fragment
+import br.digitalhouse.app_cinema.R
+
+class FragmentConta : Fragment(R.layout.fragment_conta) {
+}
---
+++ app/src/main/java/br/digitalhouse/app_cinema/fragments/FragmentFilmes.kt
@@ -0,0 +1,7 @@
+package br.digitalhouse.app_cinema.fragments
+
+import androidx.fragment.app.Fragment
+import br.digitalhouse.app_cinema.R
+
+class FragmentFilmes : Fragment(R.layout.fragment_filmes) {
+}
