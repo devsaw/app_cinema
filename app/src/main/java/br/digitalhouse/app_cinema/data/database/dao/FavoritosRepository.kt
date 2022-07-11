306358e80e733b316133025edaa4eb30212d6495
meira <meirapallaro@gmail.com>
2022-07-11 14:37:27 -0300

    alterações

---
+++ app/src/main/java/br/digitalhouse/app_cinema/data/database/dao/FavoritosRepository.kt
@@ -0,0 +1,10 @@
+package br.digitalhouse.app_cinema.data.database.dao
+
+class FavoritosRepository{
+    /*val filmesFavoritos : FilmesDatabase
+
+    suspend fun getAll(): List<Favoritos> =
+        withContext(Dispatchers.IO) {
+        filmesFavoritos.getAll()
+    }*/
+}
---
+++ app/src/main/java/br/digitalhouse/app_cinema/ui/viewmodel/FavoritosViewModel.kt
@@ -0,0 +1,13 @@
+package br.digitalhouse.app_cinema.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import br.digitalhouse.app_cinema.data.database.Favoritos
+import br.digitalhouse.app_cinema.data.database.dao.FavoritosRepository
+
+class FavoritosViewModel : ViewModel() {
+
+    //val filmesFavoritos : FavoritosRepository()
+
+  //  suspend fun getAll(): List<Favoritos> = filmesFavoritos.getAll()
+
+}
